253629a88ae35c9f72ce8d280978d95f30219913
Halyna Melnykova <galyamelnykova@gmail.com>
2022-07-12 16:28:17 +0300

    task 2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,16 +1,19 @@
 import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
 const iframe = document.querySelector('iframe');
 const player = new Player(iframe);
 
-player.on('play', function() {
-    console.log('played the video!');
-});
-
-player.getVideoTitle().then(function(title) {
-    console.log('title:', title);
-});
-
-player.on('timeupdate', function(data) {
+// записує в localStorage час не частіше, ніж раз на секунду.
+player.on('timeupdate', throttle(function(data) {
     // data is an object containing properties specific to that event
-});
+   localStorage.setItem("videoplayer-current-time", JSON.stringify(data.seconds));
+}, 1000));
+
+const savedSeconds = localStorage.getItem("videoplayer-current-time");
+
+// перевірка чи є в змінній дані
+if (savedSeconds) {
+ player.setCurrentTime(JSON.parse(savedSeconds));
+  }
+
